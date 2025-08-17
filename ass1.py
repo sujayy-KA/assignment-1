fdbe9a91557ad8124760deeb49be86ef5ec2042d
sujayy-KA <sujaybu.cct@gmail.com>
2025-08-17 13:21:34 +0530

    Add files via upload

---
+++ ass1.py
@@ -0,0 +1,7 @@
+a=input('enter first number: ')
+b=input('enter second number: ')
+
+print("The addtion is : ", int(a) + int(b))
+print("The subtraction is : ",  int(a) - int(b))
+print("The multiplication is : ",  int(a) * int(b))
+print("The division is : ",  int(a) / int(b))
